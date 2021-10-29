a) added higher-precision matrix exponential; b) fixed preservation of indices in fusion

groups.py
@@ -17,6 +17,36 @@ pi = REALTYPE('3.1415926535897932384626433832795029')
 sqrthalf = np.sqrt(REALTYPE(.5))
 # Not all functions used are compatible with higher-precision - use mpmath?
 
+# Matrix exponential accurate for high precision
+n_expm128 = 10
+eps_128 = 1e-30
+theta_n_expm128 = (eps_128*np.math.factorial(n_expm128))**(1/n_expm128)
+# Try to make matrix of order 1, to avoid squaring error
+n_expm128 = 21
+theta_n_expm128 = 1
+def expm_128(M):
+  """Matrix exponential for float128 and complex256"""
+  real = (M.dtype == REALTYPE)
+  assert real or (M.dtype == CPLXTYPE)
+  # Scaling part
+  s = max(0,int(np.ceil(np.log(linalg.norm(M)/theta_n_expm128)/np.log(2))))
+  A = M/2**s
+  # Taylor expansion part: Start with power 0
+  expA = np.identity(M.shape[0],dtype=(REALTYPE if real else CPLXTYPE))
+  Ai = A
+  for n in range(1,n_expm128):
+    expA += Ai
+    Ai = Ai.dot(A)/(n+1)
+  expA += Ai
+  # Squaring part
+  for i in range(s):
+    expA = expA.dot(expA)
+  return expA
+
+def retrieve(label):
+  """Obtain already-initialized group from label"""
+  return GroupType._registry[label]
+
 class GroupType(ABCMeta):
   _registry = {}
 
@@ -449,7 +479,7 @@ class SU2(Group):
       return 1
 
   def _firrep(self, r, g):
-    return linalg.expm(self.spinmats(r).dot(g))
+    return expm_128(self.spinmats(r).dot(g))
 
   def _firrep1d(self, r, g):
     return 1

tensors.py
@@ -228,7 +228,8 @@ class Tensor:
     Comma-separated list of indices, with identification either to
       previously-named index K, as in idx:K (or idx:K* for dual)
     or to index of argument, |#.K (optionally |# if the argument identifies a
-      single vector space or |K if exactly one tensor is provided as argument"""
+      single vector space or |K if exactly one tensor is provided as argument)
+      # indexed from zero"""
     T = np.array(T,dtype=config.FIELD)
     idxs = []
     clauses = parsestr.split(',')
@@ -849,7 +850,8 @@ class Tensor:
       for ll in set(self._idxs) - idxall:
         if ll in newidxs:
           raise ValueError('Autofilled index \'%s\' already assigned'%ll)
-        args.append((ll,(ll,)))
+        # Special form used to enforce preservation
+        args.append((ll,))
     fused,info = self._do_fuse(*args)
     if 'incl_info' in settings and not settings['incl_info']:
       return fused
@@ -862,7 +864,9 @@ class Tensor:
       if len(arg) == 2:
         l1,l0s = arg
         infos[l1] = FusionPrimitive(l0s,self)
-      else:
+      elif len(arg) == 1:
+        l1, = arg
+        infos[l1] = FusionPrimitive((l1,),self)
         assert len(arg) == 4
         l1,l0s,fusion,conj = arg
         if not isinstance(fusion,FusionPrimitive):
@@ -886,9 +890,13 @@ class Tensor:
     if prims is None: # Provided if computation is being repeated
       prims = self._get_fuse_prim(*args)
     for arg in args:
-      l1,l0s = arg[:2]
-      for ll in l0s:
-        permutation.append(self._idxs.index(ll))
+      if len(arg) == 1:
+        l1, = arg
+        permutation.append(self._idxs.index(l1))
+      else:
+        l1,l0s = arg[:2]
+        for ll in l0s:
+          permutation.append(self._idxs.index(ll))
       neworder.append(l1)
       newvs.append(prims[l1].V)
       newshape.append(prims[l1].dim)